12c73dc40521a3dc1ab205a3844da70412f74556
davidvensilinr <relaxbro2005@gmail.com>
2026-01-06 11:36:07 +0530

    navbar basic two button setup

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,6 +1,10 @@
 export default function Navbar(){
     return (
         <>
+        <nav>
+            <button>Home</button>
+            <button>Login/Signup</button>
+        </nav>
         </>
     );
 }
