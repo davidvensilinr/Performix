8f47c7cbb793a21e3302f637f75df6ad1cb20e08
davidvensilinr <relaxbro2005@gmail.com>
2026-01-07 13:49:45 +0530

    web title shifted to navbar and navbar round styling

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,7 +1,8 @@
 export default function Navbar(){
     return (
         <>
-        <nav style={{backgroundColor:'#7825ff'}} className="flex justify-between items-center p-4 bg-white">
+        <h1 className="font-sans text-center font-bold p-4 text-4xl">Performix</h1>
+        <nav style={{backgroundColor:'#7825ff'}} className="m-4 p-4 rounded-2xl h-10 flex justify-between items-center p-4 bg-white">
             <button className="font-serif text-2xl">Home</button>
             <button className="font-serif  text-2xl">Analysis-DashBoard</button>
             <button className="font-serif text-2xl">Login/Signup</button>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar"
 export default function Home() {
   return (
     <>
-    <h1 className="font-sans text-center font-bold p-4 text-4xl">Performix</h1>
+    
     <Navbar/>
     </>
   );
