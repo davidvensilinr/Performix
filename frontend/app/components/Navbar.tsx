3ad082470000701eb31240833dd2fc70decb1d6b
davidvensilinr <relaxbro2005@gmail.com>
2026-01-07 14:39:14 +0530

    linking home and signup page in navbar

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link';
 export default function Navbar(){
     return (
         <>
         <h1 className="font-sans text-center font-bold p-4 text-4xl">Performix</h1>
         <nav style={{backgroundColor:'#7825ff'}} className="m-4 p-4 rounded-2xl h-10 flex justify-between items-center p-4 bg-white">
-            <button className="font-serif text-2xl">Home</button>
+            <Link href="/"><button className="font-serif text-2xl">Home</button></Link>
             <button className="font-serif  text-2xl">Analysis-DashBoard</button>
-            <button className="font-serif text-2xl">Login/Signup</button>
+            <Link href="/pages/login"><button className="font-serif text-2xl">Login/Signup</button></Link>
         </nav>
         </>
     );
