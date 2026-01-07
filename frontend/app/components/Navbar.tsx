e852a385a7e5a487b45500d1b81391736939884e
davidvensilinr <relaxbro2005@gmail.com>
2026-01-07 13:38:49 +0530

    Navbar flex class to evenly spread the buttons

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,9 +1,10 @@
 export default function Navbar(){
     return (
         <>
-        <nav>
-            <button>Home</button>
-            <button>Login/Signup</button>
+        <nav style={{backgroundColor:'#7825ff'}} className="flex justify-between items-center p-4 bg-white">
+            <button className="font-serif text-2xl">Home</button>
+            <button className="font-serif  text-2xl">Analysis-DashBoard</button>
+            <button className="font-serif text-2xl">Login/Signup</button>
         </nav>
         </>
     );
