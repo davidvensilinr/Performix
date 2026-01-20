9b5f8b3fed42641dea2800d7dbc5a0d0e73911f8
davidvensilinr <relaxbro2005@gmail.com>
2026-01-20 12:05:46 +0530

    table objects and passing values using mapping

---
+++ frontend/app/components/org.tsx
@@ -0,0 +1,13 @@
+type props={
+    name:string;
+    managed_by:string;
+}
+export default function Org({name,managed_by}:props){
+    return(
+        <tr>
+            <td className="px-2 py-2">{name} </td>
+            <td className="px-2 py-2"> {managed_by}</td>
+            <td className="px-2 py-2 bg-white rounded-3xl hover:bg-gray-200"><button>View</button></td>
+        </tr>
+    )
+}
--- frontend/app/pages/analysis/page.tsx
+++ frontend/app/pages/analysis/page.tsx
@@ -1,11 +1,24 @@
 import Navbar from "@/app/components/Navbar"
+import Org from "@/app/components/org"
 export default function Analysis(){
+    const data =[
+        {id:1,name:"fdf",managed_by:"fdf"},
+        {id:2,name:"fdfd",managed_by:"dsnfj"}
+    ];
     return(
         <div>
         <Navbar/>
         <div className="flex flex-col items-center justify-center gap-4">
         <button className="bg-white w-1/2 h-1/2 rounded-full font-mono font-bold hover:bg-gray-300 transition duration-500 p-2 text-xl">Add your Organisation +</button>
         <h1 className=" text-white font-serif font-bold text-2xl w-full text-center">Select to View Analysis of your Organisation</h1></div>
+        <table>
+            <tbody>
+            {data.map(d=>(
+                <Org key={d.id} name={d.name} managed_by={d.managed_by}/>
+            ))}
+            
+            </tbody>
+        </table>
         </div>
     );
 }
