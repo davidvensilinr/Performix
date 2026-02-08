b871e815f15aaa69627cd16aa175158d3e6dbade
davidvensilinr <relaxbro2005@gmail.com>
2026-02-08 19:34:32 +0530

    util function to add data

--- frontend/lib/db/org.ts
+++ frontend/lib/db/org.ts
@@ -1,4 +1,10 @@
 import {prisma} from '../prisma';
+export async function addOrganisation(data:{
+    name:string,
+    managed_by:string
+}){
+    return await prisma.organisation.create({data});
+}
 export async function getAllOrganisation(){
     return await prisma.organisation.findMany();
 }
