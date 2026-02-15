5d77d0e4d1ab07eebb86f6d2ffdebbde7618c2fa
davidvensilinr <relaxbro2005@gmail.com>
2026-02-15 14:17:31 +0530

    basic organisation dashboard page setup

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar(){
         <h1 className="font-sans text-center font-bold p-4 text-4xl">Performix</h1>
         <nav style={{backgroundColor:'#7825ff'}} className="m-4 p-4 rounded-2xl h-10 flex justify-between items-center p-4 bg-white">
             <Link href="/"><button className="font-serif text-2xl text-white">Home</button></Link>
-            <button className="font-serif  text-2xl text-white">Analysis-DashBoard</button>
+            <Link href="/pages/analysis"><button className="font-serif  text-2xl text-white">Analysis-DashBoard</button></Link>
             <Link href="/pages/login"><button className="font-serif text-2xl text-white">Login/Signup</button></Link>
         </nav>
         </>
--- frontend/app/components/org.tsx
+++ frontend/app/components/org.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 type props={
     name:string;
     managed_by:string;
@@ -7,7 +8,7 @@ export default function Org({name,managed_by}:props){
         <tr>
             <td className="px-2 py-2">{name} </td>
             <td className="px-2 py-2"> {managed_by}</td>
-            <td className="px-2 py-2 bg-white rounded-3xl hover:bg-gray-200"><button>View</button></td>
+            <Link href={'/pages/organisation_dashboard'}><td className="px-2 py-2 bg-white rounded-3xl hover:bg-gray-200"><button>View</button></td></Link>
         </tr>
     )
 }
