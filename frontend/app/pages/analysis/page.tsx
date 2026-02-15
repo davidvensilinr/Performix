000e1d645400e3bd3783561be3668f9ea3ae2cea
davidvensilinr <relaxbro2005@gmail.com>
2026-02-15 14:08:06 +0530

    regular update

--- frontend/app/pages/analysis/page.tsx
+++ frontend/app/pages/analysis/page.tsx
@@ -22,7 +22,8 @@ export default function Analysis(){
     return(
         <div>
         <Navbar/>
-        <div className="flex flex-col items-center justify-center gap-4">
+        <div className="flex flex-
+    col items-center justify-center gap-4">
         <button className="bg-white w-1/2 h-1/2 rounded-full font-mono font-bold hover:bg-gray-300 transition duration-500 p-2 text-xl">Add your Organisation +</button>
         <CreateOrg/>
         <h1 className=" text-white font-serif font-bold text-2xl w-full text-center">Select to View Analysis of your Organisation</h1></div>
