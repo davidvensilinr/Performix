4aa64c4292f479b84fae210ab630a08275cc245e
davidvensilinr <relaxbro2005@gmail.com>
2026-03-19 20:05:01 +0530

    bug fix

--- backend/training/eliminate.py
+++ backend/training/eliminate.py
@@ -9,5 +9,5 @@ Y=df['fired']
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=42,test_size=0.2)
 model=LogisticRegression()
 model.fit(X_train,Y_train)
-print(accuracy_score("Trained model accuracy score :"+Y_test,model.predict(X_test))*100+"%")
+print("Trained model accuracy score :"+str(accuracy_score(Y_test,model.predict(X_test))*100)+"%")
 joblib.dump(model,"D:/Performix/backend/models/firing_model.pkl")
