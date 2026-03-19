5fb68f2ee479799462e8040b182ea2cd0d85d3e0
davidvensilinr <relaxbro2005@gmail.com>
2026-03-19 19:58:58 +0530

    logistic regression model trained and saved as pickle file for firing decision

--- backend/training/eliminate.py
+++ backend/training/eliminate.py
@@ -1,6 +1,13 @@
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import joblib
+import pandas as pd 
+df = pd.read_csv("D:/Performix/backend/dataset/employee_firing_dataset.csv")
+X=df.drop(['fired'],axis=1)
+Y=df['fired']
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=42,test_size=0.2)
 model=LogisticRegression()
 model.fit(X_train,Y_train)
+print(accuracy_score("Trained model accuracy score :"+Y_test,model.predict(X_test))*100+"%")
+joblib.dump(model,"D:/Performix/backend/models/firing_model.pkl")
