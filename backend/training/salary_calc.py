8f08db08b86f7daec8e229bbecc4a006f0e179fd
davidvensilinr <relaxbro2005@gmail.com>
2026-03-20 13:42:22 +0530

    extension bug fix

--- backend/training/salary_calc.py
+++ backend/training/salary_calc.py
@@ -10,4 +10,4 @@ X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=42,test_size=0.2
 model = LinearRegression()
 model.fit(X_train,Y_train)
 print("MSE: "+str(mean_squared_error(Y_test,model.predict(X_test))*100)+"%")
-joblib.dump(model,"D:/Performix/backend/models/salary_calculator")
+joblib.dump(model,"D:/Performix/backend/models/salary_calculator.pkl")
